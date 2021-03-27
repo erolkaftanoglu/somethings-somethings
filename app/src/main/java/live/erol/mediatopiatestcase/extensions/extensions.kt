3507b2cd1cc0243d6709f93b3ddaed6e9d934019
Erol Kaftanoglu <erolkaftanoglu@gmail.com>
2021-03-28 02:41:26 +0300

    Add utils - datastrategy, resource, safeclick and extensions

---
+++ app/src/main/java/live/erol/mediatopiatestcase/extensions/extensions.kt
@@ -0,0 +1,22 @@
+package live.erol.mediatopiatestcase.extensions
+
+import android.view.View
+import live.erol.mediatopiatestcase.utils.SafeClickListener
+
+fun View.setOnSafeClickListener(
+    onSafeClick: (View) -> Unit
+) {
+    setOnClickListener(SafeClickListener { v ->
+        onSafeClick(v)
+    })
+}
+
+fun View.setOnSafeClickListener(
+    interval: Int,
+    onSafeClick: (View) -> Unit
+) {
+    setOnClickListener(SafeClickListener(interval) { v ->
+        onSafeClick(v)
+    })
+}
+
--- app/src/main/java/live/erol/mediatopiatestcase/ui/activities/MainActivity.kt
+++ app/src/main/java/live/erol/mediatopiatestcase/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package live.erol.mediatopiatestcase
+package live.erol.mediatopiatestcase.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import live.erol.mediatopiatestcase.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
